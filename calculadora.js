87122b45b55eb73128e07c6d46ad853fcb897c6a
Daniela Irene Garzón Pinto <digarzonp@unal.edu.co>
2023-03-25 11:45:17 -0500

    Version 2.0

--- calculadora.js
+++ calculadora.js
@@ -1,4 +1,6 @@
 
+var botones=["7","8","9","+","4","5","6","-","1","2","3","*",".","=",]
+
 function renderizazGUI(){
     //Dentro de ls division de la calculadora voy a coloca
     //la division para la pantalla 
@@ -31,8 +33,42 @@ function renderizazGUI(){
       const divBotones= document.createElement("div");
       divBotones.setAttribute("id","botones");
       divCalculadora.appendChild(divBotones);
+      //vamos a crear a todos los botomes 
+      for (let i=0; i<botones.length; i++){
+        
+        if(i%4==0){
+          const divFila=document.createElement("div");
+          divFila.setAttribute("class","row"); 
+          divBotones.appendChild(divFila);
+        }
+
+        let boton=document.createElement("button");
+        boton.setAttribute("id", "boton"+botones[i]);
+        boton.setAttribute("class","btn btn-success col-3 border-white")
+        boton.innerHTML=botones[i];
+        divBotones.appendChild(boton);
+        boton.addEventListener("click",function(){
+          procesarevento(boton);
+        })
+
+      }
 
 }
+
+function procesarevento(boton){
+  let miPantalla=document.getElementById("pantalla")
+  if(miPantalla.value=="0"){
+    miPantalla.value="";
+  }
+  if(boton.innerHTML != "="){
+  miPantalla.value=miPantalla.value+boton.innerHTML;
+  }else{
+  
+    let resultado=math.evaluate(miPantalla.value);
+    miPantalla.value=resultado;
+  }
+}
+
 
 //llamar la función denderizar 
 renderizazGUI();
